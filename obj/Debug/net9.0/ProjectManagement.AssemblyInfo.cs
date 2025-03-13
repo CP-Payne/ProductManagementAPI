ec88957c2908ddbbeec8ca25597c4db18542dda7
Charles <charlethical1@gmail.com>
2025-03-13 17:13:57 +0200

    Basic app setup

--- obj/Debug/net9.0/ProjectManagement.AssemblyInfo.cs
+++ obj/Debug/net9.0/ProjectManagement.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ProjectManagement")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+dd2a2a5f6b315ea615c4ee4867e1be0032f3d17c")]
 [assembly: System.Reflection.AssemblyProductAttribute("ProjectManagement")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ProjectManagement")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
